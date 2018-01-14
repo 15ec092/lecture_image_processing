84201297441551b2d80d629d014767bf4e2ed674
Kyogo Haz <32236268+15ec092@users.noreply.github.com>
2018-01-14 14:50:55 +0900

    momijiで実行

--- kadai5.m
+++ kadai5.m
@@ -1,9 +1,8 @@
 % 課題５　判別分析法
 % 判別分析法を用いて画像二値化せよ．
-% 下記はサンプルプログラムである．
-% 課題作成にあたっては「Lenna」以外の画像を用いよ．
 
-ORG=imread('Lenna.png'); % 原画像の入力
+
+ORG=imread('momiji.png'); % 原画像の入力
 ORG = rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換
 imagesc(ORG); colormap(gray); colorbar;
 pause;
@@ -31,4 +30,3 @@ end;
 
 IMG = ORG > max_thres;
 imagesc(IMG); colormap(gray); colorbar;
-pause;
