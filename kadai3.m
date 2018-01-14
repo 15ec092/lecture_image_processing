124c618b1f877121f7d6596ae83115d854e5927c
Kyogo Haz <32236268+15ec092@users.noreply.github.com>
2018-01-14 13:22:07 +0900

    momijiで実行

--- kadai3.m
+++ kadai3.m
@@ -1,11 +1,9 @@
 % 課題３　閾値処理
 % 閾値を4パターン設定し,閾値処理た画像を示せ．
-% 下記はサンプルプログラムである．
-% 課題作成にあたっては「Lenna」以外の画像を用いよ．
 
 clear; % 変数のオールクリア
 
-ORG=imread('Lenna.png'); % 原画像の入力
+ORG=imread('momiji.png'); % 原画像の入力
 ORG= rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換
 
 imagesc(ORG); colormap(gray); colorbar; % 画像の表示
@@ -15,14 +13,14 @@ IMG = ORG > 64; % 輝度値が64以上の画素を1，その他を0に変換
 imagesc(IMG); colormap(gray); colorbar;
 pause;
 
-IMG = ORG > 96;
+IMG = ORG > 96; % 輝度値が96以上の画素を1，その他を0に変換
 imagesc(IMG); colormap(gray); colorbar;
 pause;
 
-IMG = ORG > 128;
+IMG = ORG > 128; % 輝度値が128以上の画素を1，その他を0に変換
 imagesc(IMG); colormap(gray); colorbar;
 pause;
 
-IMG = ORG > 192;
+IMG = ORG > 192; % 輝度値が192以上の画素を1，その他を0に変換
 imagesc(IMG); colormap(gray); colorbar;
 
